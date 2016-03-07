8580f5f396405f9113907df276e23248381a964a
winber1 <winber@cox.net>
2016-03-07 14:53:31 -0500

    egghead io lesson4 - state and props

---
+++ app/components/GitHub/Repos.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Repos = React.createClass({
+  render: function(){
+    return(
+        <div> 
+            <p> REPOS </p>
+            Repos: {this.props.repos}
+        </div>
+    )
+  }
+});
+
+module.exports = Repos;
--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,27 +1,33 @@
 var React = require('react');
 var Router = require('react-router');
+var Repos = require('./Github/Repos');
+var UserProfile = require('./Github/UserProfile');
+var Notes = require('./Notes/Notes');
 
 var Profile = React.createClass({
     
     getInitialState: function(){
-      return(
-            notes:[],
-            bio: {}, 
-            repos: []                     
-        )
+      return{
+            notes: [1,2,3],
+            bio: {
+                name:'winnie e'
+            }, 
+            repos: ['a','b','c']                     
+      }
     },
     
     render: function(){
+       console.log(this.props);
        return(
            <div className='row'>
              <div className='col-md-4'>
-               User Profile Component
+               <UserProfile username={this.props.params.username} bio={this.state.bio} /> 
              </div>
              <div className='col-md-4'>
-               Repos Component
+               <Repos repos={this.state.repos} />
              </div>
              <div className='col-md-4'>
-               Notes Component
+               <Notes notes={this.state.notes} />
              </div>
            </div>
         )
