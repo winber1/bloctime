f010392c008cef03db024e32ea507285aa27b127
winber1 <winber@cox.net>
2016-03-24 19:51:33 -0400

    notes reverse sort

--- app/components/Notes/NotesList.js
+++ app/components/Notes/NotesList.js
@@ -2,9 +2,11 @@ var React = require('react');
 
 var NotesList = React.createClass({
   render: function(){
-    var notes = this.props.notes.map(function(note, index){
-      return <tr  key={index}><td> {note['.value']}</td></tr>
+    var notes = this.props.notes.map(function(note)
+    {
+      return <tr  key={note.id}><td> {note['.value']}</td></tr>
     })
+    notes.reverse();
 
     return(
       <table className="table table-striped"><tbody>
